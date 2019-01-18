04e160e919e45b38fce69de508dd8b99e1e99e3f
Bryan Grill <grillb@gmail.com>
2019-01-18 11:15:01 -0500

    use css prop

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,19 +2,9 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Header } from 'semantic-ui-react';
-import styled from 'styled-components';
 
 import { WidgetContext } from './Context';
 import MyChildComponent from './MyChildComponent';
-
-const AppStyle = styled.div`
-  padding: 0.25rem;
-`;
-
-const Version = styled.div`
-  font-size: 0.7rem;
-  color: #999999ab;
-`;
 
 const MyComponent = ({ title }) => {
   const context = useContext(WidgetContext);
@@ -73,11 +63,13 @@ const MyComponent = ({ title }) => {
   }
 
   return (
-    <AppStyle>
+    <div css="padding: 0.25rem;">
       <Header>{title}</Header>
       <MyChildComponent msg={msg} open={open} />
-      <Version>Version: {context.version}</Version>
-    </AppStyle>
+      <div css="font-size: 0.7rem; color: #999999ab;">
+        Version: {context.version}
+      </div>
+    </div>
   );
 };
 
