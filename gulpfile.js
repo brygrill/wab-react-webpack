4d13c1d8c87efa9f7cab256536a43f390915dcbc
Bryan Grill <grillb@gmail.com>
2019-03-05 09:01:01 -0500

    fix #13 - run babel on deploy

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task('browserSync', () => {
 });
 
 // Copy widget folders to dist
-gulp.task('deploy', ['webpack-prod', 'other-files'], () => {
+gulp.task('deploy', ['webpack-prod', 'babel', 'other-files'], () => {
   return gulp
     .src(path.join(PATHS.dest, '/**/*'))
     .pipe(gulp.dest(path.join(PATHS.dist, PATHS.name)));
